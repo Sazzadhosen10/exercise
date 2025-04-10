54694362395f0a8bd6646085601ec1b4cb035738
Sazzadhosen10 <hosen15-5836@diu.edu.bd>
2025-04-10 13:26:17 +0600

    p3

--- lib/main.dart
+++ lib/main.dart
@@ -23,31 +23,58 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+  MySnapBar(message, context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 1),
+        content: Text(message),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+
         title: Text(
-          ' Medex', 
+          ' Medex',
           style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.bold,
             color: Colors.white,
-          ),),
-       
+          ),
+        ),
         titleSpacing: 2,
         toolbarHeight: 60,
         toolbarOpacity: 1,
         elevation: 10,
         backgroundColor: const Color.fromARGB(255, 4, 152, 110),
         actions: [
-          IconButton(onPressed: () {},icon: Icon(Icons.search),),
-          IconButton(onPressed: () {},icon: Icon(Icons.notifications),),
-          IconButton(onPressed: () {},icon: Icon(Icons.settings),),
+          IconButton(
+            onPressed: () {
+              MySnapBar("HI", context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {MySnapBar("HELLO", context);},
+            icon: Icon(Icons.notifications),
+          ),
+          IconButton(
+            onPressed: () {MySnapBar("HEY", context);},
+            icon: Icon(Icons.settings),
+          ),
         ],
       ),
-      body:Text('Welcome to My Home Page!'),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        onPressed: () => MySnapBar("Floating Action Button", context),
+        child: Icon(Icons.add),
+      ),
+      body: Text('Welcome to My Home Page!'),
     );
   }
 }
